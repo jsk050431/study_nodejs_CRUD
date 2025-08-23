42d39b083f20ffa40239aaf75089d04cc6540cbe
jsk050431_2 <jsk050431@gmail.com>
2025-08-23 13:19:49 +0900

    refactor(module): extract fileList generation into a separate module

--- getContentsListHTML.js
+++ getContentsListHTML.js
@@ -1,23 +1,4 @@
-const fs = require("fs");
-
-const fileList = {
-    getList() {
-        return new Promise((resolve, reject) => {
-            fs.readdir("./data", (err, fileList) => {
-                if (err) reject(new Error(err));
-                else resolve(fileList);
-            });
-        });
-    },
-    toHTML(fileList) {
-        let html = "";
-        fileList.forEach((fileName) => {
-            html += `<li><a href="/content/${fileName}">${fileName}</a></li>`;
-        });
-        html = `<h2><a href="/">Home</a></h2>` + "<ul>" + html + "</ul>";
-        return html;
-    },
-};
+const fileList = require("./fileList")
 
 async function getContentsListHTML() {
     const contentsList = await fileList.getList();
