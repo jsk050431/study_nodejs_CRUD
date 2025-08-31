5f6d67f927b98603fc7873b9535f2ac550dac2ad
jsk050431_2 <jsk050431@gmail.com>
2025-08-31 23:54:08 +0900

    fix(contentRouter): standardize variable naming for clarity

--- routes/contentRouter.js
+++ routes/contentRouter.js
@@ -5,14 +5,14 @@ const { getContentsListHTML } = require("../lib/fileList");
 module.exports = async function contentRouter(pathName, res) {
     try {
         const template = await fs.readFile("./views/contentView.ejs", "utf-8");
-        const _title = decodeURIComponent(pathName.split("/")[2]);
-        const content = await fs.readFile(`./data/${_title}`, "utf-8");
-        const _description = content;
+        const title = decodeURIComponent(pathName.split("/")[2]);
+        const content = await fs.readFile(`./data/${title}`, "utf-8");
+        const description = content;
         const html = ejs.render(template, {
-            title: _title,
+            title: title,
             contentsListHTML: await getContentsListHTML(),
-            contentTitle: _title,
-            description: _description,
+            contentTitle: title,
+            description: description,
         });
         res.writeHead(200, { "Content-Type": "text/html" });
         res.end(html);
